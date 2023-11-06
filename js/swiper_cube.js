0a49e9209b60da7e1a7abffc6950a673bca0ed28
mianlol01 <snake.1503@outlook.com>
2023-11-05 22:42:02 -0500

    primer avance

---
+++ js/swiper_cube.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper-container", {
+    loop: true,
+    effect: "cube",
+    autoplay: {
+      delay: 3000,
+    },
+  });
+});
